0474efe5c3b8b2e90def2c9906776904e40f3f4e
CallMeKhang <95897918+CallMeKhang@users.noreply.github.com>
2021-12-10 11:19:30 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "He luu chị!",
+  text2: "Bé có điều này muốn hỏi chị nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Chị yêu bé có phải không nào >:3",
+  text4: "Nếu chị ko trả lời mà thoát ra tức là muốn làm vợ bé rùi đó nha UwU",
+  text5: "Bé mơ à:33???",
+  text6: "Yêu ơi là yêuuu <333",
+  text7: "lí do chị thích bé đi :3333",
+  text8: "Gửi cho bé <3",
+  text9: "Vì bé dễ thương nhắmmm",
+  text10: "bé biết mà ^^ Yêu chị 3000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Tối nay bé qua đón chị đi chơi á. Còn giờ thì chờ gì nữa mà ko inbox cho bé đi nàooo",
   text12: "Okii lunn <3",
 };
 
